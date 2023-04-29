ab722338200ef9249fdd1d1f20804a9e731fd61f
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-04-28 21:11:35 -0400

    utilized inline style in order to showcase each available tier

--- frontend/src/components/Tier/TierCard.js
+++ frontend/src/components/Tier/TierCard.js
@@ -1,20 +1,22 @@
+//stateless/dumb component
+
 const TierCard = ({
     title,
     price,
     details
 }) => {
     return (
-        <>
-            <h2>{title}</h2>
-            <h4>{price}</h4>
-            <ul>
-                {details?.map((detail) => (
-                    <>
+            <div
+                style={{border:"solid black 2px", margin:"2rem", padding:"2rem", boxShadow: "10px 5px 5px steelblue"}}
+            >
+                <h2>{title}</h2>
+                <h4>${price}</h4>
+                <ul style={{display:"block"}}>
+                    {details?.map((detail) => (
                         <li>{detail}</li>
-                    </>
-                ))}
-            </ul>
-        </>
+                    ))}
+                </ul>
+            </div>
     )
 };
 
